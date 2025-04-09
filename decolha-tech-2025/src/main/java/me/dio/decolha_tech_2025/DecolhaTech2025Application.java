614e09c6afc3e09c22034a4cfda2dc64a527e5b7
paulo-rago <149490954+paulo-rago@users.noreply.github.com>
2025-04-09 19:57:41 -0300

    app

---
+++ decolha-tech-2025/src/main/java/me/dio/decolha_tech_2025/DecolhaTech2025Application.java
@@ -0,0 +1,13 @@
+package me.dio.decolha_tech_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolhaTech2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolhaTech2025Application.class, args);
+	}
+
+}
---
+++ decolha-tech-2025/src/test/java/me/dio/decolha_tech_2025/DecolhaTech2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.decolha_tech_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolhaTech2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
